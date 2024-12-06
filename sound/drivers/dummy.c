10bfd9ae32702f2a2c6ba9cfe53eb885b4afcef5
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2024-12-06 11:19:56 +0300

    WIP: ALSA: dummy: Set up more mixers to test UCM configs

--- sound/drivers/dummy.c
+++ sound/drivers/dummy.c
@@ -86,12 +86,7 @@ MODULE_PARM_DESC(hrtimer, "Use hrtimer as the timer source.");
 
 static struct platform_device *devices[SNDRV_CARDS];
 
-#define MIXER_ADDR_MASTER	0
-#define MIXER_ADDR_LINE		1
-#define MIXER_ADDR_MIC		2
-#define MIXER_ADDR_SYNTH	3
-#define MIXER_ADDR_CD		4
-#define MIXER_ADDR_LAST		4
+#define MIXER_COUNT		16
 
 struct dummy_timer_ops {
 	int (*create)(struct snd_pcm_substream *);
@@ -128,11 +123,15 @@ struct snd_dummy {
 	struct snd_pcm *pcm;
 	struct snd_pcm_hardware pcm_hw;
 	spinlock_t mixer_lock;
-	int mixer_volume[MIXER_ADDR_LAST+1][2];
-	int capture_source[MIXER_ADDR_LAST+1][2];
-	int iobox;
-	struct snd_kcontrol *cd_volume_ctl;
-	struct snd_kcontrol *cd_switch_ctl;
+	int playback_volume[MIXER_COUNT+1][2];
+	int capture_volume[MIXER_COUNT+1][2];
+	int playback_switch[MIXER_COUNT+1][2];
+	int capture_switch[MIXER_COUNT+1][2];
+	/*
+	 * int iobox;
+	 * struct snd_kcontrol *cd_volume_ctl;
+	 * struct snd_kcontrol *cd_switch_ctl;
+	 */
 };
 
 /*
@@ -699,16 +698,16 @@ static int snd_card_dummy_pcm(struct snd_dummy *dummy, int device,
  * mixer interface
  */
 
-#define DUMMY_VOLUME(xname, xindex, addr) \
+#define DUMMY_PLYVOL(xname, xindex, addr) \
 { .iface = SNDRV_CTL_ELEM_IFACE_MIXER, \
   .access = SNDRV_CTL_ELEM_ACCESS_READWRITE | SNDRV_CTL_ELEM_ACCESS_TLV_READ, \
   .name = xname, .index = xindex, \
-  .info = snd_dummy_volume_info, \
-  .get = snd_dummy_volume_get, .put = snd_dummy_volume_put, \
+  .info = snd_dummy_plyvol_info, \
+  .get = snd_dummy_plyvol_get, .put = snd_dummy_plyvol_put, \
   .private_value = addr, \
   .tlv = { .p = db_scale_dummy } }
 
-static int snd_dummy_volume_info(struct snd_kcontrol *kcontrol,
+static int snd_dummy_plyvol_info(struct snd_kcontrol *kcontrol,
 				 struct snd_ctl_elem_info *uinfo)
 {
 	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
@@ -718,20 +717,20 @@ static int snd_dummy_volume_info(struct snd_kcontrol *kcontrol,
 	return 0;
 }
 
-static int snd_dummy_volume_get(struct snd_kcontrol *kcontrol,
+static int snd_dummy_plyvol_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
 	int addr = kcontrol->private_value;
 
 	spin_lock_irq(&dummy->mixer_lock);
-	ucontrol->value.integer.value[0] = dummy->mixer_volume[addr][0];
-	ucontrol->value.integer.value[1] = dummy->mixer_volume[addr][1];
+	ucontrol->value.integer.value[0] = dummy->playback_volume[addr][0];
+	ucontrol->value.integer.value[1] = dummy->playback_volume[addr][1];
 	spin_unlock_irq(&dummy->mixer_lock);
 	return 0;
 }
 
-static int snd_dummy_volume_put(struct snd_kcontrol *kcontrol,
+static int snd_dummy_plyvol_put(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
@@ -749,38 +748,97 @@ static int snd_dummy_volume_put(struct snd_kcontrol *kcontrol,
 	if (right > mixer_volume_level_max)
 		right = mixer_volume_level_max;
 	spin_lock_irq(&dummy->mixer_lock);
-	change = dummy->mixer_volume[addr][0] != left ||
-	         dummy->mixer_volume[addr][1] != right;
-	dummy->mixer_volume[addr][0] = left;
-	dummy->mixer_volume[addr][1] = right;
+	change = dummy->playback_volume[addr][0] != left ||
+	         dummy->playback_volume[addr][1] != right;
+	dummy->playback_volume[addr][0] = left;
+	dummy->playback_volume[addr][1] = right;
 	spin_unlock_irq(&dummy->mixer_lock);
 	return change;
 }
 
-static const DECLARE_TLV_DB_SCALE(db_scale_dummy, -4500, 30, 0);
+#define DUMMY_CAPVOL(xname, xindex, addr) \
+{ .iface = SNDRV_CTL_ELEM_IFACE_MIXER, \
+  .access = SNDRV_CTL_ELEM_ACCESS_READWRITE | SNDRV_CTL_ELEM_ACCESS_TLV_READ, \
+  .name = xname, .index = xindex, \
+  .info = snd_dummy_capvol_info, \
+  .get = snd_dummy_capvol_get, .put = snd_dummy_capvol_put, \
+  .private_value = addr, \
+  .tlv = { .p = db_scale_dummy } }
 
-#define DUMMY_CAPSRC(xname, xindex, addr) \
-{ .iface = SNDRV_CTL_ELEM_IFACE_MIXER, .name = xname, .index = xindex, \
-  .info = snd_dummy_capsrc_info, \
-  .get = snd_dummy_capsrc_get, .put = snd_dummy_capsrc_put, \
-  .private_value = addr }
+static int snd_dummy_capvol_info(struct snd_kcontrol *kcontrol,
+				 struct snd_ctl_elem_info *uinfo)
+{
+	uinfo->type = SNDRV_CTL_ELEM_TYPE_INTEGER;
+	uinfo->count = 2;
+	uinfo->value.integer.min = mixer_volume_level_min;
+	uinfo->value.integer.max = mixer_volume_level_max;
+	return 0;
+}
 
-#define snd_dummy_capsrc_info	snd_ctl_boolean_stereo_info
-
-static int snd_dummy_capsrc_get(struct snd_kcontrol *kcontrol,
+static int snd_dummy_capvol_get(struct snd_kcontrol *kcontrol,
 				struct snd_ctl_elem_value *ucontrol)
 {
 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
 	int addr = kcontrol->private_value;
 
 	spin_lock_irq(&dummy->mixer_lock);
-	ucontrol->value.integer.value[0] = dummy->capture_source[addr][0];
-	ucontrol->value.integer.value[1] = dummy->capture_source[addr][1];
+	ucontrol->value.integer.value[0] = dummy->capture_volume[addr][0];
+	ucontrol->value.integer.value[1] = dummy->capture_volume[addr][1];
 	spin_unlock_irq(&dummy->mixer_lock);
 	return 0;
 }
 
-static int snd_dummy_capsrc_put(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
+static int snd_dummy_capvol_put(struct snd_kcontrol *kcontrol,
+				struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
+	int change, addr = kcontrol->private_value;
+	int left, right;
+
+	left = ucontrol->value.integer.value[0];
+	if (left < mixer_volume_level_min)
+		left = mixer_volume_level_min;
+	if (left > mixer_volume_level_max)
+		left = mixer_volume_level_max;
+	right = ucontrol->value.integer.value[1];
+	if (right < mixer_volume_level_min)
+		right = mixer_volume_level_min;
+	if (right > mixer_volume_level_max)
+		right = mixer_volume_level_max;
+	spin_lock_irq(&dummy->mixer_lock);
+	change = dummy->capture_volume[addr][0] != left ||
+	         dummy->capture_volume[addr][1] != right;
+	dummy->capture_volume[addr][0] = left;
+	dummy->capture_volume[addr][1] = right;
+	spin_unlock_irq(&dummy->mixer_lock);
+	return change;
+}
+
+static const DECLARE_TLV_DB_SCALE(db_scale_dummy, -4500, 30, 0);
+
+#define DUMMY_PLYSW(xname, xindex, addr) \
+{ .iface = SNDRV_CTL_ELEM_IFACE_MIXER, .name = xname, .index = xindex, \
+  .info = snd_dummy_plysw_info, \
+  .get = snd_dummy_plysw_get, .put = snd_dummy_plysw_put, \
+  .private_value = addr }
+
+#define snd_dummy_plysw_info	snd_ctl_boolean_stereo_info
+
+static int snd_dummy_plysw_get(struct snd_kcontrol *kcontrol,
+				  struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
+	int addr = kcontrol->private_value;
+
+	spin_lock_irq(&dummy->mixer_lock);
+	ucontrol->value.integer.value[0] = dummy->playback_switch[addr][0];
+	ucontrol->value.integer.value[1] = dummy->playback_switch[addr][1];
+	spin_unlock_irq(&dummy->mixer_lock);
+	return 0;
+}
+
+static int snd_dummy_plysw_put(struct snd_kcontrol *kcontrol,
+				  struct snd_ctl_elem_value *ucontrol)
 {
 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
 	int change, addr = kcontrol->private_value;
@@ -789,83 +847,144 @@ static int snd_dummy_capsrc_put(struct snd_kcontrol *kcontrol, struct snd_ctl_el
 	left = ucontrol->value.integer.value[0] & 1;
 	right = ucontrol->value.integer.value[1] & 1;
 	spin_lock_irq(&dummy->mixer_lock);
-	change = dummy->capture_source[addr][0] != left &&
-	         dummy->capture_source[addr][1] != right;
-	dummy->capture_source[addr][0] = left;
-	dummy->capture_source[addr][1] = right;
+	change = dummy->playback_switch[addr][0] != left ||
+	         dummy->playback_switch[addr][1] != right;
+	dummy->playback_switch[addr][0] = left;
+	dummy->playback_switch[addr][1] = right;
 	spin_unlock_irq(&dummy->mixer_lock);
 	return change;
 }
 
-static int snd_dummy_iobox_info(struct snd_kcontrol *kcontrol,
-				struct snd_ctl_elem_info *info)
-{
-	static const char *const names[] = { "None", "CD Player" };
+#define DUMMY_CAPSW(xname, xindex, addr) \
+{ .iface = SNDRV_CTL_ELEM_IFACE_MIXER, .name = xname, .index = xindex, \
+  .info = snd_dummy_capsw_info, \
+  .get = snd_dummy_capsw_get, .put = snd_dummy_capsw_put, \
+  .private_value = addr }
 
-	return snd_ctl_enum_info(info, 1, 2, names);
-}
+#define snd_dummy_capsw_info	snd_ctl_boolean_stereo_info
 
-static int snd_dummy_iobox_get(struct snd_kcontrol *kcontrol,
-			       struct snd_ctl_elem_value *value)
+static int snd_dummy_capsw_get(struct snd_kcontrol *kcontrol,
+				struct snd_ctl_elem_value *ucontrol)
 {
 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
+	int addr = kcontrol->private_value;
 
-	value->value.enumerated.item[0] = dummy->iobox;
+	spin_lock_irq(&dummy->mixer_lock);
+	ucontrol->value.integer.value[0] = dummy->capture_switch[addr][0];
+	ucontrol->value.integer.value[1] = dummy->capture_switch[addr][1];
+	spin_unlock_irq(&dummy->mixer_lock);
 	return 0;
 }
 
-static int snd_dummy_iobox_put(struct snd_kcontrol *kcontrol,
-			       struct snd_ctl_elem_value *value)
+static int snd_dummy_capsw_put(struct snd_kcontrol *kcontrol, struct snd_ctl_elem_value *ucontrol)
 {
 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
-	int changed;
+	int change, addr = kcontrol->private_value;
+	int left, right;
 
-	if (value->value.enumerated.item[0] > 1)
-		return -EINVAL;
-
-	changed = value->value.enumerated.item[0] != dummy->iobox;
-	if (changed) {
-		dummy->iobox = value->value.enumerated.item[0];
-
-		if (dummy->iobox) {
-			dummy->cd_volume_ctl->vd[0].access &=
-				~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
-			dummy->cd_switch_ctl->vd[0].access &=
-				~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
-		} else {
-			dummy->cd_volume_ctl->vd[0].access |=
-				SNDRV_CTL_ELEM_ACCESS_INACTIVE;
-			dummy->cd_switch_ctl->vd[0].access |=
-				SNDRV_CTL_ELEM_ACCESS_INACTIVE;
-		}
-
-		snd_ctl_notify(dummy->card, SNDRV_CTL_EVENT_MASK_INFO,
-			       &dummy->cd_volume_ctl->id);
-		snd_ctl_notify(dummy->card, SNDRV_CTL_EVENT_MASK_INFO,
-			       &dummy->cd_switch_ctl->id);
-	}
-
-	return changed;
+	left = ucontrol->value.integer.value[0] & 1;
+	right = ucontrol->value.integer.value[1] & 1;
+	spin_lock_irq(&dummy->mixer_lock);
+	change = dummy->capture_switch[addr][0] != left ||
+	         dummy->capture_switch[addr][1] != right;
+	dummy->capture_switch[addr][0] = left;
+	dummy->capture_switch[addr][1] = right;
+	spin_unlock_irq(&dummy->mixer_lock);
+	return change;
 }
 
+/* static int snd_dummy_iobox_info(struct snd_kcontrol *kcontrol,
+ * 				struct snd_ctl_elem_info *info)
+ * {
+ * 	static const char *const names[] = { "None", "CD Player" };
+ *
+ * 	return snd_ctl_enum_info(info, 1, 2, names);
+ * }
+ *
+ * static int snd_dummy_iobox_get(struct snd_kcontrol *kcontrol,
+ * 			       struct snd_ctl_elem_value *value)
+ * {
+ * 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
+ *
+ * 	value->value.enumerated.item[0] = dummy->iobox;
+ * 	return 0;
+ * }
+ *
+ * static int snd_dummy_iobox_put(struct snd_kcontrol *kcontrol,
+ * 			       struct snd_ctl_elem_value *value)
+ * {
+ * 	struct snd_dummy *dummy = snd_kcontrol_chip(kcontrol);
+ * 	int changed;
+ *
+ * 	if (value->value.enumerated.item[0] > 1)
+ * 		return -EINVAL;
+ *
+ * 	changed = value->value.enumerated.item[0] != dummy->iobox;
+ * 	if (changed) {
+ * 		dummy->iobox = value->value.enumerated.item[0];
+ *
+ * 		if (dummy->iobox) {
+ * 			dummy->cd_volume_ctl->vd[0].access &=
+ * 				~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+ * 			dummy->cd_switch_ctl->vd[0].access &=
+ * 				~SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+ * 		} else {
+ * 			dummy->cd_volume_ctl->vd[0].access |=
+ * 				SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+ * 			dummy->cd_switch_ctl->vd[0].access |=
+ * 				SNDRV_CTL_ELEM_ACCESS_INACTIVE;
+ * 		}
+ *
+ * 		snd_ctl_notify(dummy->card, SNDRV_CTL_EVENT_MASK_INFO,
+ * 			       &dummy->cd_volume_ctl->id);
+ * 		snd_ctl_notify(dummy->card, SNDRV_CTL_EVENT_MASK_INFO,
+ * 			       &dummy->cd_switch_ctl->id);
+ * 	}
+ *
+ * 	return changed;
+ * }
+ */
+
 static const struct snd_kcontrol_new snd_dummy_controls[] = {
-DUMMY_VOLUME("Master Volume", 0, MIXER_ADDR_MASTER),
-DUMMY_CAPSRC("Master Capture Switch", 0, MIXER_ADDR_MASTER),
-DUMMY_VOLUME("Synth Volume", 0, MIXER_ADDR_SYNTH),
-DUMMY_CAPSRC("Synth Capture Switch", 0, MIXER_ADDR_SYNTH),
-DUMMY_VOLUME("Line Volume", 0, MIXER_ADDR_LINE),
-DUMMY_CAPSRC("Line Capture Switch", 0, MIXER_ADDR_LINE),
-DUMMY_VOLUME("Mic Volume", 0, MIXER_ADDR_MIC),
-DUMMY_CAPSRC("Mic Capture Switch", 0, MIXER_ADDR_MIC),
-DUMMY_VOLUME("CD Volume", 0, MIXER_ADDR_CD),
-DUMMY_CAPSRC("CD Capture Switch", 0, MIXER_ADDR_CD),
-{
-	.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
-	.name  = "External I/O Box",
-	.info  = snd_dummy_iobox_info,
-	.get   = snd_dummy_iobox_get,
-	.put   = snd_dummy_iobox_put,
-},
+DUMMY_PLYVOL("A Playback Volume", 0, 0),
+DUMMY_PLYSW("A Playback Switch", 0, 0),
+DUMMY_CAPVOL("A Capture Volume", 0, 0),
+DUMMY_CAPSW("A Capture Switch", 0, 0),
+
+DUMMY_PLYVOL("B Playback Volume", 0, 1),
+DUMMY_PLYSW("B Playback Switch", 0, 1),
+DUMMY_CAPVOL("B Capture Volume", 0, 1),
+DUMMY_CAPSW("B Capture Switch", 0, 1),
+
+DUMMY_PLYVOL("C Playback Volume", 0, 2),
+DUMMY_PLYSW("C Playback Switch", 0, 2),
+DUMMY_CAPVOL("C Capture Volume", 0, 2),
+DUMMY_CAPSW("C Capture Switch", 0, 2),
+
+DUMMY_PLYVOL("D Playback Volume", 0, 3),
+DUMMY_PLYSW("D Playback Switch", 0, 3),
+DUMMY_CAPVOL("D Capture Volume", 0, 3),
+DUMMY_CAPSW("D Capture Switch", 0, 3),
+
+DUMMY_PLYSW("X Playback Switch", 0, 4),
+DUMMY_CAPVOL("X Capture Volume", 0, 4),
+DUMMY_CAPSW("X Capture Switch", 0, 4),
+
+DUMMY_PLYVOL("Y Playback Volume", 0, 5),
+DUMMY_PLYSW("Y Playback Switch", 0, 5),
+DUMMY_CAPSW("Y Capture Switch", 0, 5),
+
+DUMMY_PLYSW("Z Playback Switch", 0, 6),
+DUMMY_CAPSW("Z Capture Switch", 0, 6),
+/*
+ * {
+ * 	.iface = SNDRV_CTL_ELEM_IFACE_MIXER,
+ * 	.name  = "Z Box",
+ * 	.info  = snd_dummy_iobox_info,
+ * 	.get   = snd_dummy_iobox_get,
+ * 	.put   = snd_dummy_iobox_put,
+ * },
+ */
 };
 
 static int snd_card_dummy_new_mixer(struct snd_dummy *dummy)
@@ -877,17 +996,21 @@ static int snd_card_dummy_new_mixer(struct snd_dummy *dummy)
 
 	spin_lock_init(&dummy->mixer_lock);
 	strcpy(card->mixername, "Dummy Mixer");
-	dummy->iobox = 1;
+	/* dummy->iobox = 1; */
 
 	for (idx = 0; idx < ARRAY_SIZE(snd_dummy_controls); idx++) {
 		kcontrol = snd_ctl_new1(&snd_dummy_controls[idx], dummy);
 		err = snd_ctl_add(card, kcontrol);
 		if (err < 0)
 			return err;
-		if (!strcmp(kcontrol->id.name, "CD Volume"))
-			dummy->cd_volume_ctl = kcontrol;
-		else if (!strcmp(kcontrol->id.name, "CD Capture Switch"))
-			dummy->cd_switch_ctl = kcontrol;
+		/*
+		 * if (!strcmp(kcontrol->id.name, "Z Playback Volume"))
+		 * 	dummy->cd_volume_ctl = kcontrol;
+		 * else if (!strcmp(kcontrol->id.name, "Z Playback Switch"))
+		 * 	dummy->cd_switch_ctl = kcontrol;
+		 * else if (!strcmp(kcontrol->id.name, "Z Capture Switch"))
+		 * 	dummy->cd_switch_ctl = kcontrol;
+		 */
 
 	}
 	return 0;
